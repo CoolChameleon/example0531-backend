15ad0f980b60caad19415c3c4703d45fa746295f
hanchang <hanchang2016@pku.edu.cn>
2023-05-31 09:44:15 +0000

    commit all these moxxxxfuxxxxs

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'text': 'Hello!'})
+
 @app.route('/app/add', methods=['POST'])
 def add():
     data = request.get_json()
